098ce13998ff2711b6bc0ad46c6f558473e0146e
Kephren <kephren.delannay.sampany@gmail.com>
2022-09-20 16:10:29 -0400

    Finished

--- exercice.py
+++ exercice.py
@@ -4,21 +4,53 @@
 import math
 
 def get_num_letters(text):
-	return 0
+	result = 0
+	for char in text:
+		result += 1 if str.isalnum(char) else 0
+	return result
 
 def get_word_length_histogram(text):
-	return [0]
+	hist = [0]
+	words = text.split()
+	test = []
+	for w in words:
+		n = get_num_letters(w)
+		test.append(n)
+		if len(hist) < n + 1:
+			to_add = (n) - len(hist) + 1
+			add = [0 for x in range(to_add)]
+			hist += add
+			hist[n] += 1
+		else:
+			hist[n] += 1
+	return hist
 
 def format_histogram(histogram):
 	ROW_CHAR = "*"
-
-	return ""
+	result = ""
+	for index in range(1, len(histogram)):
+		value = histogram[index]
+		result += f'{index:>2}' + ' ' + ROW_CHAR * value + '\n'
+	return result
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
+	result = ""
 
-	return ""
+	highest = histogram.copy()
+	highest.sort()
+	highest = highest[-1]
+
+	for i in range(highest, 0, -1):
+		for j in range(1, len(histogram)):
+			if histogram[j] >= i:
+				result += BLOCK_CHAR
+			else:
+				result += ' '
+		result += '\n'
+	result += LINE_CHAR * len(histogram)
+	return result
 
 
 if __name__ == "__main__":
